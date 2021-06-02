bfea3960e7ce4fe093e87af300247e60daeb45a3
Jesus <jesuscopc@gmail.com>
2021-06-02 09:18:40 -0500

    chore: Initial projects configuration with typescript

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+// settings
+app.set('port', process.env.PORT || 3000);
+
+// middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// routes
+app.use(require('./routes'));
+
+// starting the server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
