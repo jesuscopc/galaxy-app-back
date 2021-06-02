e908e1b7bd7cfb2c015821e95b2b6bc9c5064cd7
Jesus <jesuscopc@gmail.com>
2021-06-02 14:14:45 -0500

    chore: Allow CORS get data from any site

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,14 @@ app.use(require('./routes'));
 app.use('/api/topsecret', require('./routes/topsecret'));
 app.use('/api/topsecret_split', require('./routes/topsecret_split'));
 
+//CORS dont do this in a production environemtn
+app.use(function(req: any, res: any, next: any) {
+  res.header("Access-Control-Allow-Origin", "*"); // use only to develop enviroment
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, OPTIONS");
+  res.header("X-Frame-Options", "SAMEORIGIN");
+  next();
+});
 // starting the server
 app.listen(app.get('port'), () => {
     console.log(`Server on port ${app.get('port')}`);
