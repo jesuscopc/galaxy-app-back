58f50692efe378d4a4dcb77acfebb7712e2f2c49
Jesus <jesuscopc@gmail.com>
2021-06-02 14:26:59 -0500

    chore: CORS

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
+
+let whitelist = ['http://localhost:4000', 'http://abc.com']
 
 // settings
 app.set('port', process.env.PORT || 4000);
@@ -16,12 +19,12 @@ app.use('/api/topsecret', require('./routes/topsecret'));
 app.use('/api/topsecret_split', require('./routes/topsecret_split'));
 
 //CORS dont do this in a production environemtn
-app.use(function(req: any, res: any, next: any) {
-  res.header("Access-Control-Allow-Origin", "*"); // use only to develop enviroment
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, OPTIONS");
-  res.header("X-Frame-Options", "SAMEORIGIN");
-  next();
+
+app.use(cors());
+app.use(function(req: any, res: any, next: any) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 // starting the server
 app.listen(app.get('port'), () => {
