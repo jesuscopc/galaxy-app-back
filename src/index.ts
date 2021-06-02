066709d66d04997828fc10f434c0db37627c8844
Jesus <jesuscopc@gmail.com>
2021-06-02 10:17:32 -0500

    chore: change to port 4000

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const morgan = require('morgan');
 const app = express();
 
 // settings
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 4000);
 
 // middlewares
 app.use(morgan('dev'));
